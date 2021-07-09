2122770e55358c68a9ffa4e515e4722f88d8ee1c
Huanyu <huanyu.li@liu.se>
2021-07-09 18:27:51 +0200

    Update

--- schema_generator/graphql_schema_gen.py
+++ schema_generator/graphql_schema_gen.py
@@ -36,7 +36,7 @@ def read_TBox(TBox_file = './TBox.yml'):
         #print(data)
     return data['concepts'], data['data_types'], data['data_properties'], data['object_properties'], data['subsumptions'], data['axioms']
 def write_file(schema_file, gq_schema):
-    with open(schema_file, 'w') as output_file:
+    with open(schema_file, 'w+') as output_file:
         output_file.write(gq_schema)
     #with open('./schemas/' + schema_file, 'w') as output_file:
 
@@ -102,7 +102,166 @@ class GraphQLSchema(object):
         self.implementation = defaultdict(list)
         self.implementation_temp = defaultdict(list)
         self.fields = defaultdict(dict)
-    
+        self.concept2superconcepts = dict()
+
+        self.I_T = list()
+        self.O_T = list()
+        self.IO_T = list()
+        self.F = list()
+        self.S = list()
+
+        self.type_S_F = defaultdict(dict)
+        self.type_S_I_F = defaultdict(dict)
+        self.type_S_AF = defaultdict(dict)
+        self.impl = defaultdict(list)
+
+    @staticmethod
+    def Phi(term):
+        if term in V2Scalar.keys():
+            new_term = V2Scalar[term]
+        else:
+            new_term = remove_prefix(term)
+        return new_term
+
+    @staticmethod
+    def Psi(entry):
+        return '{entry_type}List'.format(entry_type=remove_prefix(entry))
+
+    @staticmethod
+    def Upsilon(interface_name):
+        return '{interface}_IF'.format(interface=remove_prefix(interface_name))
+
+    @staticmethod
+    def Theta(entry):
+        return '{entry_type}Filter'.format(entry_type=remove_prefix(entry))
+
+    @staticmethod
+    def check_assertion_cardinality(assertions):
+        card_flag = False
+        for assertion in assertions:
+            if assertion[2] == '=1':
+                card_flag = True
+                break
+        return card_flag
+
+    def schema_gen(self, N_C, N_D, N_A, N_R, concept2superconcept, concept2assertions):
+        self.O_T = [self.Phi(c) for c in N_C]
+        self.IO_T = [self.Theta(c) for c in N_C]
+        #self.fields[concept][(assertion[0], field_type)] = (min_card, max_card)
+        for c in N_C:
+            self.type_S_F[self.Phi(c)][('IRI', 'String')] = (1, 1)
+            self.type_S_F['Query'][(self.Psi(c), self.Phi(c))] =(0, float('inf'))
+            self.type_S_AF['Query'][(self.Psi(c), 'filter')] = (1, 1)
+        self.F = [self.Phi(r) for r in N_R]
+        self.F += [self.Phi(a) for a in N_A]
+        self.S = [self.Phi(d) for d in N_D]
+        self.IO_T += [self.Theta(self.Phi(d)) for d in N_D]
+        for sub_concept, super_concepts in concept2superconcept.items():
+            self.concept2superconcepts[remove_prefix(sub_concept)] = [remove_prefix(sc) for sc in super_concepts]
+            for super_concept in super_concepts:
+                self.I_T.append(self.Upsilon(super_concept))
+                self.type_S_F[self.Upsilon(super_concept)][('IRI', 'String')] = (1, 1)
+                self.impl[self.Upsilon(super_concept)].append(self.Phi(sub_concept))
+                if self.Phi(super_concept) not in self.impl[self.Upsilon(super_concept)]:
+                    self.impl[self.Upsilon(super_concept)].append(self.Phi(super_concept))
+        for (concept, assertions) in concept2assertions.items():
+            for assertion in assertions:
+                min_card = 0
+                max_card = float("inf")
+                if assertion[2] == '>=0':
+                    if self.check_assertion_cardinality(assertions) is True:
+                        max_card = 1
+                field_type = assertion[1]
+                if assertion[1] in V2Scalar.keys():
+                    field_type = V2Scalar[assertion[1]]
+                self.type_S_F[self.Phi(concept)][(self.Phi(assertion[0]), self.Phi(field_type))] = (min_card, max_card)
+                self.type_S_I_F[self.Theta(concept)][(self.Phi(assertion[0]), self.Theta(field_type))] = (min_card, 1)
+        # self.I = list(set(self.I))
+        # print(self.impl)
+        return
+
+    def render_object_types(self):
+        object_type_str_dict = defaultdict()
+        object_impl_str_dict = defaultdict()
+        for object_type, field_definition_dict in self.type_S_F.items():
+            if object_type not in self.I_T:
+                impl_str = ''
+                if object_type in self.concept2superconcepts.keys():
+                    interfaces_lst = [self.Upsilon(sc) for sc in self.concept2superconcepts[object_type]]
+                    impl_str = ' & '.join(interfaces_lst)
+                    object_impl_str_dict[object_type] = ' implements {impl_str}'.format(impl_str=impl_str)
+                line_str_lst = ['{\n']
+                for field_name_type, cards in field_definition_dict.items():
+                    field_name = field_name_type[0]
+                    field_type = field_name_type[1]
+                    if cards[1] == 1:
+                        field_return_str = '\t{field_name}: {return_type}\n'.format(field_name=field_name, return_type=field_type)
+                    else:
+                        if object_type == 'Query':
+                            field_return_str = '\t{field_name}(filter: {input_type}): [{return_type}]\n'.format(field_name=field_name, input_type=self.Theta(field_type), return_type=field_type)
+                        else:
+                            field_return_str = '\t{field_name}: [{return_type}]\n'.format(field_name=field_name,
+                                                                                        return_type=field_type)
+                    line_str_lst.append(field_return_str)
+                line_str_lst.append('}\n')
+                object_type_str_dict[object_type] = line_str_lst
+        with open('test-schema.graphql', 'a') as output_file:
+            for object_type, object_field_lst in object_type_str_dict.items():
+                if object_type in object_impl_str_dict.keys():
+                    # print('type ' + object_type + object_impl_str_dict[object_type])
+                    output_file.write('type ' + object_type + object_impl_str_dict[object_type])
+                else:
+                    # print('type ' + object_type)
+                    output_file.write('type ' + object_type)
+                # print(''.join(object_field_lst))
+                output_file.write(''.join(object_field_lst))
+
+    def render_interface_types(self):
+        interface_type_str_dict = defaultdict()
+        for interface_name in self.impl.keys():
+            object_type_name = interface_name.split('_IF')[0]
+            field_definition_dict = self.type_S_F[object_type_name]
+            line_str_lst = ['{\n']
+            for field_name_type, cards in field_definition_dict.items():
+                field_name = field_name_type[0]
+                field_type = field_name_type[1]
+                if cards[1] == 1:
+                    field_return_str = '\t{field_name}: {return_type}\n'.format(field_name=field_name,
+                                                                                return_type=field_type)
+                else:
+                    field_return_str = '\t{field_name}: [{return_type}]\n'.format(field_name=field_name,
+                                                                                  return_type=field_type)
+                line_str_lst.append(field_return_str)
+            line_str_lst.append('}\n')
+            interface_type_str_dict[interface_name] = line_str_lst
+        with open('test-schema.graphql', 'a') as output_file:
+            for object_type, object_field_lst in interface_type_str_dict.items():
+                # print('interface ' + object_type)
+                # print(''.join(object_field_lst))
+                output_file.write('interface ' + object_type)
+                output_file.write(''.join(object_field_lst))
+
+    def render_input_object_types(self):
+        input_object_type_str_dict = defaultdict()
+        for object_type, field_definition_dict in self.type_S_I_F.items():
+            line_str_lst = ['{\n']
+            for field_name_type, cards in field_definition_dict.items():
+                field_name = field_name_type[0]
+                field_type = field_name_type[1]
+                if cards[1] == 1:
+                    field_return_str = '\t{field_name}: {return_type}\n'.format(field_name=field_name, return_type=field_type)
+                else:
+                    field_return_str = '\t{field_name}: [{return_type}]\n'.format(field_name=field_name, return_type=field_type)
+                line_str_lst.append(field_return_str)
+            line_str_lst.append('}\n')
+            input_object_type_str_dict[object_type] = line_str_lst
+        with open('test-schema.graphql', 'a') as output_file:
+            for object_type, object_field_lst in input_object_type_str_dict.items():
+                # print('input ' + object_type)
+                # print(''.join(object_field_lst))
+                output_file.write('input ' + object_type)
+                output_file.write(''.join(object_field_lst))
+
     def construct(self, A, V, U, P, concept2superconcept, concept2assertions):
         self.I = A
         self.SC = V
@@ -110,6 +269,12 @@ class GraphQLSchema(object):
         self.RF = P
         #self.implementation = concept2subsumptions
         self.implementation = concept2superconcept
+        '''
+        for sub_concept, super_concepts in concept2superconcept.items():
+            for super_concept in super_concepts:
+                self.I.append(super_concept)
+        self.I = list(set(self.I))
+        '''
         for (concept, assertions) in concept2assertions.items():
             for assertion in assertions:
                 min_card = 0
@@ -196,8 +361,11 @@ class GraphQLSchema(object):
         print(interfacetype_schema)
         print(objecttype_schema)
         print(querytype_schema)
-        write_file('./schema_generator/schema-input_type-' + global_ontolopy_name +'.graphql',interfacetype_schema)
-        write_file('./schema_generator/schema-object_type-' + global_ontolopy_name +'.graphql',objecttype_schema+'\n'+querytype_schema)
+        # write_file('./schema_generator/schema-input_type-' + global_ontolopy_name +'.graphql',interfacetype_schema)
+        # write_file('./schema_generator/schema-object_type-' + global_ontolopy_name +'.graphql',objecttype_schema+'\n'+querytype_schema)
+        write_file('schema-input_type-' + global_ontolopy_name + '.graphql', interfacetype_schema)
+        write_file('schema-object_type-' + global_ontolopy_name + '.graphql',
+                   objecttype_schema + '\n' + querytype_schema)
 
     def write_local_schema(self, local_prefixes):
         for local_prefix in local_prefixes:
@@ -265,8 +433,12 @@ def main(ontology):
     graphql_schema_test = GraphQLSchema()
     #graphql_schema_test.construct(elq1d_test.A, elq1d_test.V, elq1d_test.U, elq1d_test.P, elq1d_test.concept2subsumptions, elq1d_test.concept2assertions)
     graphql_schema_test.construct(A, V, U, P, concepts2superconcepts, concepts2axioms)
-    graphql_schema_test.write_global_schema()
-    graphql_schema_test.write_OntologyGraphQLSchemaMapping(A, U, P)
+    graphql_schema_test.schema_gen(A, V, U, P, concepts2superconcepts, concepts2axioms)
+    graphql_schema_test.render_interface_types()
+    graphql_schema_test.render_object_types()
+    graphql_schema_test.render_input_object_types()
+    # graphql_schema_test.write_global_schema()
+    # graphql_schema_test.write_OntologyGraphQLSchemaMapping(A, U, P)
     #graphql_schema_test.write_local_schema(['OQMD','MP'])
     #graphql_schema_test.write_local_schema(databases)
     b = datetime.datetime.now()
